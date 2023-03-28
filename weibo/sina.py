3da6c39b0ccf51073f8b8edaec84ce392809a0bb
cnmeein <root@instance-2.us-central1-a.c.alien-oarlock-380804.internal>
2023-03-28 02:36:36 +0000

    push gcp tw

---
+++ weibo/sina.py
@@ -0,0 +1,24 @@
+import datetime
+import requests
+import os
+
+response = requests.get("https://api-new2020.vercel.app/weibo")
+
+data = response.json()
+
+now = datetime.datetime.now()
+
+date_string = now.strftime("%m.%d_%H:%M")
+
+dir_path = "/root/Hot/weibo"
+if not os.path.exists(dir_path):
+    os.mkdir(dir_path)
+file_path = os.path.join(dir_path, f"{date_string}.md")
+
+with open(file_path, "w", encoding="utf-8") as f:
+
+    for item in data["data"]:
+        title = item["title"]
+        url = item["url"]
+
+        f.write(f"[{title}]({url})\n\n")
